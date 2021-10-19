8c0bfc8e78f6c4f5c9184b091f74f1a626e14c2e
molly <nmcaleenan07@qub.ac.uk>
2021-10-19 12:20:21 +0100

    //test lines added

--- src/uk/ac/qub/eeecs/dave/Calculator.java
+++ src/uk/ac/qub/eeecs/dave/Calculator.java
@@ -2,11 +2,13 @@ package uk.ac.qub.eeecs.dave;
 
 public class Calculator {
 	public int add(int a, int b) {
-		return a+b+1;
+		return a+b;
 	}
 	
 	public int sub(int a, int b) {
 		return a-b;
-	} 
-	
+	} 	
 }
+
+//hello this is an added line
+// this is an added line 2
